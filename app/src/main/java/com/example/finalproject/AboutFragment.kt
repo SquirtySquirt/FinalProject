d4fbb4d6fda874ffcfe39ab89ebafa9415e43d74
Conor Hatton <conorhatton07@gmail.com>
2024-05-01 13:00:17 -0400

    everything

--- app/src/main/java/com/example/finalproject/AboutFragment.kt
+++ app/src/main/java/com/example/finalproject/AboutFragment.kt
@@ -1,20 +1,47 @@
 package com.example.finalproject
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.finalproject.databinding.FragmentAboutBinding
+import com.example.finalproject.databinding.FragmentMainBinding
 
 
 class AboutFragment : Fragment() {
-
+    private var _binding: FragmentAboutBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
+
+        binding.phoneButton.setOnClickListener{
+            val callIntent: Intent = Uri.parse("tel:5709834589").let { number ->
+                Intent(Intent.ACTION_DIAL, number)
+
+
+            }
+        }
+        binding.mailButton.setOnClickListener{
+            Intent(Intent.ACTION_SEND).apply {
+                // The intent does not have a URI, so declare the "text/plain" MIME type
+                type = "text/plain"
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("bakera@ahsd.org "))
+                putExtra(Intent.EXTRA_SUBJECT, "Email subject")
+                putExtra(Intent.EXTRA_TEXT, "Email message text")
+                putExtra(Intent.EXTRA_STREAM, Uri.parse("content://path/to/email/attachment"))
+
+            }
+        }
+
+
+
+        return binding.root
     }
 
 
